7af28a19449f97415e8dde8d4fa9ef0e08567723
Mark Opondo <markopondoi.t@gmail.com>
2026-03-22 17:05:24 +0300

    updateProductName function

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,23 @@
 // Write your code here
+// Create a products array with string values
+const products = ["Laptop", "Phone", "Headphones", "Monitor"]
 
+// Function to get the first item in the array
+const logFirstProduct = () => {
+  let firstProduct = products[0]
+  console.log(firstProduct) // Console log the details of the first product
+  return firstProduct
+}
+
+// Function to update an item in the array 
+const updateProductName = (index, name) => {
+  // products.splice(index, 1, name)
+  products[index] = name
+  console.log(products)
+}
+
+logFirstProduct()
+updateProductName(0, "Table")
 
 
 // Export the necessary parts for testing
